4df4285ea6d88f3cdac3099fc7d325a76dcd80bf
TheRealOkGaming <therealokgaming@gmail.com>
2024-09-10 13:00:19 -0400

    Update server.js

--- server.js
+++ server.js
@@ -22,6 +22,23 @@ redisClient.on('error', (err) => {
     console.error('Redis error:', err);
 });
 
+redisClient.on('ready', () => {
+    console.log('Redis client connected');
+});
+
+redisClient.on('end', () => {
+    console.log('Redis client disconnected');
+});
+
+// Ensure client is connected before using
+(async () => {
+    try {
+        await redisClient.connect();
+    } catch (err) {
+        console.error('Failed to connect to Redis:', err);
+    }
+})();
+
 app.use(express.json());
 
 // Configure CORS options
